73d6966bc29e288b8e7e813c5e7dac23dc395cd4
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-10-08 01:10:15 +0100

    chore: fixes

--- futaverse/settings.py
+++ futaverse/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    # "PAGE_SIZE": 10,
+    # "PAGE_SIZE": 10,1
     # "PAGE_SIZE_QUERY_PARAM": "size",
 }
 
