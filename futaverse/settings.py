63da785bee8eefc92d7a04e808b928e27b14cf5b
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-10-08 01:10:08 +0100

    chore: fixes

--- futaverse/settings.py
+++ futaverse/settings.py
@@ -10,18 +10,11 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1&^(s=j&v^w$2o6^g-8nv@z!l)0!9cmt^be4jn5!oo$gcw89rq'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["futaverse-backend-1.onrender.com", "localhost", "127.0.0.1",]
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
